d8255bbda7fbf56fa1c9d5d280a1cc38f511aa09
Matthew Yoshida <mchangyoshida@gmail.com>
2021-06-01 01:34:01 -0700

    removed folder server

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 
 const express = require('express');
-const cors = require('cors');
 const app = express();
 
-app.use(cors());
 app.use('/api', (req, res) => {
     res.send({
       token: 'test123'
