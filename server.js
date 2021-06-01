e2a1f71caeb8886da8a465fa7a47f26cab53bed5
Matthew Yoshida <mchangyoshida@gmail.com>
2021-06-01 08:07:30 -0700

    updated server

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
+const express = require("express");
 
-const express = require('express');
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
-app.use('/api', (req, res) => {
-    res.send({
-      token: 'test123'
-    });
-  });
-
-  app.listen(3000, () => console.log('API is running on http://localhost:3000/api'));
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
