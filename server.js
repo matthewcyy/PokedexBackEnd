f2d272348142cd071aa6fd4879218262eaf7d4de
Matthew Yoshida <mchangyoshida@gmail.com>
2021-06-01 08:20:31 -0700

    Added app.use

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use('/api', (req, res) => {
+  res.send({
+    token: 'test123'
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
