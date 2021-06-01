b37c81b5dcb48928bdc0c63b1f54de40da4ece5d
Matthew Yoshida <mchangyoshida@gmail.com>
2021-06-01 08:41:26 -0700

    added cors and use cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(cors());
 
 app.use('/api', (req, res) => {
   res.send({
