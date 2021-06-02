0237d3e634e7bf3bf43b387850d2cb2f1bf0fffb
Matthew Yoshida <mchangyoshida@gmail.com>
2021-06-02 07:13:09 -0700

    created config variables and edited procfile for heroku

--- server.js
+++ server.js
@@ -12,11 +12,10 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
-const uri = 'mongodb+srv://matthewcy:9rxcFZ3WN4nehXus@cluster0.dgsat.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 // set up mongoose
 mongoose.connect(
-  uri,
+  process.env.MONGODB_CONNECTION_STRING,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
